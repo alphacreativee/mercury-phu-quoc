9863fb608bc9bf751bb245c8c7a760ce4d119b4e
Gary <hai.alphacreative@gmail.com>
2025-07-06 11:42:25 +0700

    add src

---
+++ assets/js/index/script.js
@@ -0,0 +1,89 @@
+import { preloadImages } from "../../libs/utils.js";
+// setup lenis
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+// end lenis
+
+// dropdown
+
+function customDropdown() {
+  const dropdowns = document.querySelectorAll(".dropdown-custom");
+
+  dropdowns.forEach((dropdown) => {
+    const btnDropdown = dropdown.querySelector(".dropdown-custom-btn");
+    const dropdownMenu = dropdown.querySelector(".dropdown-custom-menu");
+    const dropdownItems = dropdown.querySelectorAll(".dropdown-custom-item");
+    const valueSelect = dropdown.querySelector(".value-select");
+
+    // Toggle dropdown on button click
+    btnDropdown.addEventListener("click", function (e) {
+      e.stopPropagation();
+      closeAllDropdowns(dropdown);
+      dropdownMenu.classList.toggle("dropdown--active");
+      btnDropdown.classList.toggle("--active");
+    });
+
+    // Close dropdown when clicking outside
+    document.addEventListener("click", function () {
+      closeAllDropdowns();
+    });
+
+    // Handle item selection
+    dropdownItems.forEach((item) => {
+      item.addEventListener("click", function (e) {
+        e.stopPropagation();
+
+        // Store current values from the button
+        const currentImg = valueSelect.querySelector("img").src;
+        const currentText = valueSelect.querySelector("span").textContent;
+        const currentHtml = valueSelect.innerHTML;
+
+        // Store clicked item values
+        const clickedHtml = item.innerHTML;
+
+        // Update the button with clicked item values
+        valueSelect.innerHTML = clickedHtml;
+
+        // Update the clicked item with the previous button values
+        item.innerHTML = `<img src="${currentImg}" alt="" /><span>${currentText}</span>`;
+
+        closeAllDropdowns();
+      });
+    });
+
+    // Close dropdown on scroll
+    window.addEventListener("scroll", function () {
+      closeAllDropdowns();
+    });
+  });
+
+  function closeAllDropdowns(exception) {
+    dropdowns.forEach((dropdown) => {
+      const menu = dropdown.querySelector(".dropdown-custom-menu");
+      const btn = dropdown.querySelector(".dropdown-custom-btn");
+
+      if (!exception || dropdown !== exception) {
+        menu.classList.remove("dropdown--active");
+        btn.classList.remove("--active");
+      }
+    });
+  }
+}
+// end dropdown
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  customDropdown();
+};
+preloadImages("img").then(() => {
+  // Once images are preloaded, remove the 'loading' indicator/class from the body
+
+  init();
+});
+$(window).on("beforeunload", function () {
+  $(window).scrollTop(0);
+});
